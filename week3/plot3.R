5452dd3a78609427ed85f35fb3bc9bf3af7ab0db
Jared <jaredsun26@gmail.com>
2015-04-29 16:17:30 -0700

    use dplyr to solve Q3

--- week3/plot3.R
+++ week3/plot3.R
@@ -1,4 +1,5 @@
 library(ggplot2)
+library(dplyr)
 #download data
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip",destfile="data.zip")
 unzip("./data.zip")
@@ -7,12 +8,11 @@ pm25<- readRDS("./summarySCC_PM25.rds")
 scc<- readRDS("./Source_Classification_Code.rds")
 
 #emission of Baltimore city only
-pm25_BC<-pm25[pm25$fips=="24510",]
+pm25_BC<-filter(pm25,fips=="24510")%>% group_by(type,year)%>%summarise(totalEmissions=sum(Emissions))
+
 
 #plot
 png("plot3.png")
-g<-ggplot(pm25_BC, aes(year, Emissions, color=type))
-g+stat_summary(geom="line", fun.y="sum")+
-  ylab("Total PM2.5 Emission (ton)")+
-  ggtitle("Total PM2.5 emission in Baltimore City from 1999 to 2008")
+g<-ggplot(pm25_BC, aes(year, totalEmissions, color=type))
+g+geom_line()
 dev.off()
